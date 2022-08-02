f51c0d42e7501af01cb8da7f49a2059efa0d0c83
Gabriel Lisboa <gabrielbento.rx@outlook.com>
2022-08-02 13:46:20 -0300

    feat: Implement optional authorization for routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { WalletController } from './wallet/wallet.controller';
 import { CryptumService } from './cryptum/cryptum.service';
 import { TransactionController } from './transaction/transaction.controller';
 import { PricesController } from './prices/prices.controller';
 import { BlockController } from './block/block.controller';
+import { AuthMiddleware } from './auth.middleware';
 
 @Module({
   controllers: [AppController, WalletController, TransactionController, PricesController, BlockController],
   providers: [CryptumService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
